8b46ebde8c982004e924f5a6fc122e20c7ab5baf
liziwei01 <alssylk@gmail.com>
2023-05-12 17:39:23 -0400

    Initial Commit

---
+++ Monitor/MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorApp.swift
+//  Monitor
+//
+//  Created by 李子为 on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
